b2a43cb3233e80cdad687f6632403a4465cdf4bc
Priyeshu Garg <priyeshu1@gmail.com>
2020-06-09 23:27:26 +0530

    changed git urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/qredo/documentation',
             },
           ],
         },
@@ -87,13 +87,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/qredo/documentation',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/qredo/documentation',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
