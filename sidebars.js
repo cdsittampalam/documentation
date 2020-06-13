6e8f2aa081095a264703a0473297fb48b951b8d2
Brian Spector <brianspector@apache.org>
2020-06-13 15:35:58 +0100

    fix sidebard.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,13 @@
 module.exports = {
   someSidebar: {
-    Installation: ['doc1', 'doc2', 'doc3'],
-    Trading: ['test', 'mdx'],
+    Learn: ['doc1', 'doc2', 'doc3'],
+    Product Guides: ['test', 'mdx'],
+    Customer Care: ['test', 'mdx'],
+    Network Members: ['test', 'mdx'],
+    Partners: ['test', 'mdx'],
+    Deployment Guides: ['test', 'mdx'],
+    Network Status: ['test', 'mdx'],
+    Opensource: ['test', 'mdx'],
+    Roadmap: ['test', 'mdx'],
   },
 };
