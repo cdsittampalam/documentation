ecc7e0369192cd210e7e0f401ff1ee45d5166f29
Brian Spector <brianspector@apache.org>
2020-06-14 11:53:23 +0100

    fix js

--- sidebars.js
+++ sidebars.js
@@ -1,12 +1,14 @@
 module.exports = {
   someSidebar: {
     Learn: ['Qredo Network', 'Security & Cryptography', 'Regulatory & Compliance'],
-    'Product Guide': ['doc1', 'mdx'],
-    'Customer Care': ['test', 'mdx'],
-    Members: ['test', 'mdx'],
+    'Product Guide': ['Getting Started', 'Configure Accounts', 'Fund Administration','Deposit and Withdraw'],
+    'Customer Care': ['Account Help', 'Transaction Fees', 'Contact Us', 'FAQ'],
+    'Network Members': ['test', 'mdx'],
     Partners: ['test', 'mdx'],
     Deployment: ['test', 'mdx'],
     Opensource: ['test', 'mdx'],
     Roadmap: ['test', 'mdx'],
+    Test1: ['test', 'mdx'],
+    Test2: ['doc1','doc2','doc3']
   },
 };
