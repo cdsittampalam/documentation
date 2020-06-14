2639dddef146215f75b8e2c433f03f64bc573e27
Brian Spector <brianspector@apache.org>
2020-06-14 12:13:24 +0100

    more sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,11 @@
 module.exports = {
   someSidebar: {
     Learn: ['Qredo Network', 'Security & Cryptography', 'Regulatory & Compliance'],
-    'Product Guide': ['Getting Started', 'Configure Accounts', 'Fund Administration','Deposit and Withdraw'],
     'Customer Care': ['Account Help', 'Transaction Fees', 'Contact Us', 'FAQ'],
-    'Network Members': ['test', 'mdx'],
+    'Network Members': ['doc1', 'doc2', 'doc3'],
     Partners: ['test', 'mdx'],
     Deployment: ['test', 'mdx'],
     Opensource: ['test', 'mdx'],
-    Roadmap: ['test', 'mdx'],
-    Test1: ['test', 'mdx'],
-    Test2: ['doc1','doc2','doc3']
+    Roadmap: ['test', 'mdx']
   },
 };
