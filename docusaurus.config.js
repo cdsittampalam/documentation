b454ecbd301d2fc3c6c155526aa657fd4b3be0c5
Milan Darjanin <milan.darjanin@gmail.com>
2020-06-23 19:05:33 +0200

    Update visual of the landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,102 +1,100 @@
 module.exports = {
-  title: 'Qredo Help Center',
-  tagline: 'Knowledge Base and Customer Support for Qredos',
-  url: 'https://thronecloud.github.io',
-  baseUrl: '/',
-  favicon: '/img/favicon.ico',
-  organizationName: 'qredo', // Usually your GitHub org/user name.
-  projectName: 'documentation', // Usually your repo name.
+  title: "Qredo Help Center",
+  tagline: "Knowledge Base and Customer Support for Qredos",
+  url: "https://thronecloud.github.io",
+  baseUrl: "/",
+  favicon: "/img/favicon.ico",
+  organizationName: "qredo", // Usually your GitHub org/user name.
+  projectName: "documentation", // Usually your repo name.
   themeConfig: {
+    disableDarkMode: true,
+    defaultDarkMode: false,
     algolia: {
-      apiKey: 'f0598b74cf278ea9e04592f0d7db3736',
-      indexName: 'thronecloud'
+      apiKey: "f0598b74cf278ea9e04592f0d7db3736",
+      indexName: "thronecloud",
     },
     navbar: {
-      title: 'Qredo',
       logo: {
-        alt: 'Qredo Logo',
-        src: '/img/q_logo.png',
+        alt: "Qredo Logo",
+        src: "/img/qredo-horizonal-logo.svg",
       },
       links: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: "blog", label: "Blog", position: "left" },
       ],
     },
     footer: {
-      style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'What is Qredo',
-              to: 'docs/',
+              label: "What is Qredo",
+              to: "docs/",
             },
             {
-              label: 'Getting Started with Qredo',
-              to: 'docs/doc2/',
+              label: "Getting Started with Qredo",
+              to: "docs/doc2/",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Qredo',
-              href: 'https://www.qredo.com/',
+              label: "Qredo",
+              href: "https://www.qredo.com/",
             },
             {
-              label: 'Apache Milagro',
-              href: 'https://milagro.apache.org/users.html',
+              label: "Apache Milagro",
+              href: "https://milagro.apache.org/users.html",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/QredoNetwork',
+              label: "Twitter",
+              href: "https://twitter.com/QredoNetwork",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/qredo/documentation',
+              label: "GitHub",
+              href: "https://github.com/qredo/documentation",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Qredo Support.`,
+      copyright: `Copyright &copy; ${new Date().getFullYear()} Qredo Support.`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'Qredo Network',
-          sidebarPath: require.resolve('./sidebars.js'),
+          homePageId: "Qredo Network",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/qredo/documentation/tree/master/',
+          editUrl: "https://github.com/qredo/documentation/tree/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/qredo/documentation/tree/master/',
+          editUrl: "https://github.com/qredo/documentation/tree/master/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
