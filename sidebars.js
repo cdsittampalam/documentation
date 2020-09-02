3007805bd561c86f5c7b4f9e16714b342ab9ac88
kdslang <ken@qredo.com>
2020-09-02 16:47:38 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     Overview: ['Qredo Network', 'Security & Cryptography', 'Regulatory & Compliance'],
     'Product Guide': ['Qredo App Overview', 'Getting Started', 'Configure Account', 'Invite Trusted Parties', 'Fund Administration', 'Custodian Administration', 'Deposit & Withdraw', 'Creating Transactions'],
-    'Customer Care': ['Account Help', 'Account Recovery', 'Payment Help', 'Plans & Pricing', 'Transaction Fees', 'Contact Us', 'FAQ'],
+    'Customer Care': ['Account Help', 'Account Recovery', 'Payment Help', 'Plans & Pricing', 'Transaction Fees', 'Contact Us','FAQ'],
     'Network Members': ['Financial Institutions', 'Exchanges', 'Custodians', 'Traders'],
     Partners: ['Systems Integrators', 'EMS & OMS ISVs', 'Partner API'],
     'Deployment Guides': ['Custodian App', 'Custodian Server', 'Qredo Validators', 'Qredo Server'],
